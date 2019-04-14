6ee8cca8d4a29c9482ec89aac80c3ac019183164
Jason Graziano <mrehawk82@gmail.com>
2019-04-14 19:55:08 -0400

    doin stuff

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,6 @@
-var myUI;
+var myUI, gameData;
+
+gameData: () => {};
 
 myUI = {
 	init: ()=>{
@@ -8,69 +10,68 @@ myUI = {
 		myUI.buildEle();
 	},
 	buildEle: ()=>{
+
         var eles = createEle("div"), 
             cv = createEle("canvas"),
             ctx = cv.getContext('2d'),
             btnFrame = createEle("div");
-        
-        cv.width = 496;
-        cv.height = 496;
+        const cH = window.innerHeight, cW = window.innerWidth;
 
-        myUI.loop(cv,ctx);
-        myUI.generateBtns(btnFrame);
+        cv.width = cW;
+        cv.height = cH;
+btnFrame.className = "btnFrame";
+        //myUI.loop(cv,ctx);
+        myUI.generateBtns(cv, btnFrame);
+
         eles.append(cv, btnFrame);
         
 		dvContain.append(eles);
 	},
-	generateBtns: (btnFrame) => {
+	generateBtns: (cv, btnFrame)=>{
+        var btn1 = createEle("button"),
+            btn2 = createEle("button");
 
+        btn1.innerHTML = "end turn";
+        btn1.className = "btns";
+        btn1.disabled = true;
+        
+        btn2.innerHTML = "+";
+        btn2.className = "btns";
+        btn2.disabled = false;
+        btn2.onclick = myUI.addCell(cv, btn2);
+
+        btnFrame.append(btn1, btn2);
+	},
+	addCell: (cv, btn2) => {
+ 		return ()=>{
+ 			cv.onclick = myUI.cellPlotter(cv);
+ 			btn2.disabled = true;
+ 		}
+	},
+	cellPlotter: (cv) => {
+		return ()=>{
+			console.log(cv);
+		}
 	},
     loop: (cv,ctx)=>{
-        var ctx = cv.getContext('2d');
-  ctx.fillRect(0, 0, 150, 150);
-  ctx.translate(75, 75);
-
-  // Create a circular clipping path
-  ctx.beginPath();
-  ctx.arc(0, 0, 60, 0, Math.PI * 4, true);
-  ctx.clip();
-
-  // draw background
-  var lingrad = ctx.createLinearGradient(0, -75, 0, 75);
-  lingrad.addColorStop(0, '#232256');
-  lingrad.addColorStop(1, '#143778');
-  
-  ctx.fillStyle = lingrad;
-  ctx.fillRect(-75, -75, 150, 150);
-
-  // draw stars
-  for (var j = 1; j < 50; j++) {
-    ctx.save();
-    ctx.fillStyle = '#fff';
-    ctx.translate(75 - Math.floor(Math.random() * 150),
-                  75 - Math.floor(Math.random() * 150));
-    drawStar(ctx, Math.floor(Math.random() * 1) + 2);
-    ctx.restore();
-  }
-
-function drawStar(ctx, r) {
-  ctx.save();
-  ctx.beginPath();
-  ctx.moveTo(r, 0);
-  for (var i = 0; i < 16; i++) {
-    ctx.rotate(Math.PI / 5);
-    if (i % 2 === 0) {
-      ctx.lineTo((r / 0.525731) * 0.200811, 0);
-    } else {
-      ctx.lineTo(r, 0);
+    	ctx.beginPath();
+  		ctx.moveTo(50,50);
+  	    ctx.lineTo(100,100);
+  	    ctx.stroke();
     }
-  }
-  ctx.closePath();
-  ctx.fill();
-  ctx.restore();
-};
-}
 };
 window.onload = ()=>{
 	myUI.init();
+};
+window.onresize = (cv)=>{
+	if(cv) {
+		if(cv.width != body.clientWidth){
+    		dvContain.innerHTML = "";
+			myUI.init();
+    	}else {
+return false;
+    }
+	} else {
+		myUI.init();
+	}
 };
